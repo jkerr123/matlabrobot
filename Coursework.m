2de38c4dd08728228e800b0aecd47f37334efc96
jamiekerr <jamiekerr@labpc444.computing.dundee.ac.uk>
2016-03-11 14:01:21 +0000

    Moved window comparison to PIXEL_DISP

--- Coursework.m
+++ Coursework.m
@@ -13,44 +13,12 @@ searchWindow = getSearchWindow(imageR, startX,startY,15);
 
 
 
-%Searching for the closest match. 
-closestMatch = [];
-smallestSSD= [];
- xcord=[];
- ycord=[];
 
-for y=1:15-3
-   for x=1:15-3
-     
-    RightWindow  = getSupportWindow(searchWindow,x,y,3);  
-    SSD = getSSD(supportWindow,RightWindow);
-    
-    if isempty(closestMatch)
-     
-        closestMatch = RightWindow;
-    end
-    
-    if isempty(smallestSSD) || SSD < smallestSSD
-        smallestSSD = SSD;
-        closestMatch = RightWindow;
-        
-  
-        xcord = x;
-        ycord = y;
-    end
-%     'Print'
-     closestMatch
-%     smallestSSD
-    
-   end
-end
-
- xcord = startX + (xcord - 7);
- ycord = startY + (ycord -7);
  
- pixelDisp = PIXEL_DISP(startX,startY,xcord,ycord)
+ [pixelDisp, window, SSD] = PIXEL_DISP(searchWindow, supportWindow, startX, startY)
  
- imageMap = getDispMap(pixelDisp,imageL,imageR)
+ 
+ imageMap = getDispMap(pixelDisp,imageL,imageR);
 
 
 %getSSD(supportWindow, supportWindow2)
--- PIXEL_DISP.m
+++ PIXEL_DISP.m
@@ -1,8 +1,45 @@
-function DISP = PIXEL_DISP(xl,yl,xr,yr)
+function [DISP, smallestSSD, closestMatch] = PIXEL_DISP(searchWindow, supportWindow, startX, startY)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-x = xl - xr;
-y = yl - yr;
+
+%Searching for the closest match. 
+closestMatch = [];
+smallestSSD= [];
+ xcord=[];
+ ycord=[];
+
+for y=1:15-3
+   for x=1:15-3
+     
+    RightWindow  = getSupportWindow(searchWindow,x,y,3);  
+    SSD = getSSD(supportWindow,RightWindow);
+    
+    if isempty(closestMatch)
+     
+        closestMatch = RightWindow;
+    end
+    
+    if isempty(smallestSSD) || SSD < smallestSSD
+        smallestSSD = SSD;
+        closestMatch = RightWindow;
+        
+  
+        xcord = x;
+        ycord = y;
+    end
+%     'Print'
+%     closestMatch
+%     smallestSSD
+    
+   end
+end
+
+ xcord = startX + (xcord - 7);
+ ycord = startY + (ycord -7);
+ 
+ x = startX - xcord
+ y = startY - ycord
+
 
 DISP =[x,y];
 end
